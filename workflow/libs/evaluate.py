a3bc6b7dbfab43bf7596c7ecd0eed9baac229c03
evansna <nathaniel.evansj@gmail.com>
2020-10-06 08:12:43 -0700

    results interp; refactored plotting in evaluate.py

--- workflow/libs/evaluate.py
+++ workflow/libs/evaluate.py
@@ -3,6 +3,31 @@ import segment
 from matplotlib import pyplot as plt
 
 
+def plot_registration(fixed, moving, fixed_seg=None, moving_seg=None, figsize=(10,15)):
+    '''
+    '''
+    if fixed_seg is None: 
+        fixed_seg  = segment.perform_otsu_threshold(fixed)
+    
+    if moving_seg is None: 
+        moving_seg = segment.perform_otsu_threshold(moving)
+    
+    # make sure not unsigned, we want to allow negative values 
+    fixed_seg_show = sitk.Cast(sitk.RescaleIntensity(fixed_seg), sitk.sitkInt8)
+    moving_seg_show = sitk.Cast(sitk.RescaleIntensity(moving_seg), sitk.sitkInt8)
+
+    f, axes = plt.subplots(1,3, figsize=figsize)
+    axes[0].imshow(sitk.GetArrayViewFromImage(fixed_seg_show))
+    axes[0].set_title('fixed segmentation')
+    axes[1].imshow(sitk.GetArrayViewFromImage(moving_seg_show))
+    axes[1].set_title('moving segmentation')
+    diff = fixed_seg_show - moving_seg_show
+    axes[2].imshow(sitk.GetArrayViewFromImage(diff))
+    axes[2].set_title('difference')
+    plt.tight_layout()
+    plt.show()
+    
+
 def eval_registration(fixed, moving, name, plot=True): 
     '''
     
@@ -18,21 +43,7 @@ def eval_registration(fixed, moving, name, plot=True):
         
     if plot: 
         print('review the segmentations to make sure the look appropriate for comparison...')
-        
-        # make sure not unsigned, we want to allow negative values 
-        fixed_seg_show = sitk.Cast(sitk.RescaleIntensity(fixed_seg), sitk.sitkInt8)
-        moving_seg_show = sitk.Cast(sitk.RescaleIntensity(moving_seg), sitk.sitkInt8)
-
-        f, axes = plt.subplots(1,3, figsize=(10,15))
-        axes[0].imshow(sitk.GetArrayViewFromImage(fixed_seg_show))
-        axes[0].set_title('fixed segmentation')
-        axes[1].imshow(sitk.GetArrayViewFromImage(moving_seg_show))
-        axes[1].set_title('moving segmentation')
-        diff = fixed_seg_show - moving_seg_show
-        axes[2].imshow(sitk.GetArrayViewFromImage(diff))
-        axes[2].set_title('difference')
-        plt.tight_layout()
-        plt.show()
+        plot_registration(fixed, moving, fixed_seg=fixed_seg, moving_seg=moving_seg)
 
     overlap_measures_filter.Execute(fixed_seg, moving_seg)
     reg_eval['jacaard_coef'] = overlap_measures_filter.GetJaccardCoefficient()
