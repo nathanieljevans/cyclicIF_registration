fdb7ba25c51723dcd12bdfba26c183bc6284792d
evansna <nathaniel.evansj@gmail.com>
2020-10-15 08:37:19 -0700

    functional - 11.9% failure rate

--- workflow/libs/evaluate.py
+++ workflow/libs/evaluate.py
@@ -1,7 +1,7 @@
 import SimpleITK as sitk 
 import segment
 from matplotlib import pyplot as plt
-
+import utils
 
 def plot_registration(fixed, moving, fixed_seg=None, moving_seg=None, figsize=(10,15)):
     '''
@@ -15,15 +15,12 @@ def plot_registration(fixed, moving, fixed_seg=None, moving_seg=None, figsize=(1
     # make sure not unsigned, we want to allow negative values 
     fixed_seg_show = sitk.Cast(sitk.RescaleIntensity(fixed_seg), sitk.sitkInt8)
     moving_seg_show = sitk.Cast(sitk.RescaleIntensity(moving_seg), sitk.sitkInt8)
+    diff = fixed_seg_show - moving_seg_show
 
     f, axes = plt.subplots(1,3, figsize=figsize)
-    axes[0].imshow(sitk.GetArrayViewFromImage(fixed_seg_show))
-    axes[0].set_title('fixed segmentation')
-    axes[1].imshow(sitk.GetArrayViewFromImage(moving_seg_show))
-    axes[1].set_title('moving segmentation')
-    diff = fixed_seg_show - moving_seg_show
-    axes[2].imshow(sitk.GetArrayViewFromImage(diff))
-    axes[2].set_title('difference')
+    utils.myshow(fixed_seg_show, title='fixed', ax=axes[0])
+    utils.myshow(moving_seg_show, title='moving', ax=axes[1])
+    utils.myshow(diff, title='difference', ax=axes[2])
     plt.tight_layout()
     plt.show()
     
